19efdcbc4307f51c0d66b79e18380770f31eff4e
Aravindargutus <118649483+Aravindargutus@users.noreply.github.com>
2025-01-30 17:44:02 +0530

    Update AppError.js

--- functions/zoho_analytics_datastore_sync_routes_handler/utils/AppError.js
+++ functions/zoho_analytics_datastore_sync_routes_handler/utils/AppError.js
@@ -1,11 +1,17 @@
 class AppError extends Error {
-  statusCode = 500
+  statusCode = 500;
 
-  constructor (statusCode, message) {
-    super()
+  constructor(statusCode, message) {
+    super();
 
-    this.statusCode = statusCode
-    this.message = message
+    // Check if statusCode is null or undefined
+    if (statusCode == null) { // This checks for both null and undefined
+      throw new Error('statusCode cannot be null or undefined');
+    }
+
+    this.statusCode = statusCode;
+    this.message = message;
   }
 }
-module.exports = AppError
+
+module.exports = AppError;
